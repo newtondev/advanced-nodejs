368f0c55335fcb3b0eb11696e5eace98aad8783d
Craig Newton <newtondev@gmail.com>
2018-08-15 09:16:13 +0200

    Added initial code.

---
+++ 00_performance/03_multitask.js
@@ -0,0 +1,36 @@
+// Tell libuv to only create 2 threads for the thread pool (default = 4 threads)
+process.env.UV_THREADPOOL_SIZE = 4;
+
+const https = require('https');
+const crypto = require('crypto');
+const fs = require('fs');
+
+const start = Date.now();
+
+function doRequest() {
+  https
+    .request('https://www.google.com', res => {
+      res.on('data', () => {});
+      res.on('end', () => {
+        console.log(Date.now() - start);
+      });
+    })
+    .end();
+}
+
+function doHash() {
+  crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    console.log('Hash:', Date.now() - start);
+  });
+}
+
+doRequest();
+
+fs.readFile('multitask.js', 'utf8', () => {
+  console.log('FS:', Date.now() - start);
+});
+
+doHash();
+doHash();
+doHash();
+doHash();
---
+++ 00_performance/05_pm2usage.js
@@ -0,0 +1,18 @@
+// run: pm2 start index -i 0
+// should start up load balancer with app x logical processors
+
+const express = require('express');
+const crypto = require('crypto');
+const app = express();
+
+app.get('/', (req, res) => {
+  crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    res.send('Hi there');
+  });
+});
+
+app.get('/fast', (req, res) => {
+  res.send('This was fast!');
+});
+
+app.listen(3000);
